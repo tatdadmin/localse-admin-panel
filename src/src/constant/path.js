4d20c71dc86ba875d1dc615c98d62e786a3f7ed7
POOJA SHARMA <poojasharma@POOJAs-MacBook-Pro.local>
2025-05-26 11:41:15 +0530

    add hourly report

--- src/src/constant/path.js
+++ src/src/constant/path.js
@@ -1,4 +1,4 @@
-// export const API_BASE_URL = "/api/";
+export const API_BASE_URL = "/api/";
 // export const API_BASE_URL = "http://15.206.117.178:5001/api/";
 // export const API_BASE_URL = "https://gx82jqhb-5001.inc1.devtunnels.ms/api/"
 
